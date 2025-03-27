20aed6ade9b74cc5dedfff3bb980f34e82f28552
Devon Crebbin <devon@land.org.au>
2025-03-27 16:42:26 +0000

    updated max actions limit

--- soh/include/z64player.h
+++ soh/include/z64player.h
@@ -163,10 +163,10 @@ typedef enum PlayerItemAction {
     /* 0x40 */ PLAYER_IA_MASK_GERUDO,
     /* 0x41 */ PLAYER_IA_MASK_TRUTH,
     /* 0x42 */ PLAYER_IA_LENS_OF_TRUTH,
-    /* 0x43 */ PLAYER_IA_MAX,
 
     // CUSTOM
-    PLAYER_IA_GLIDER,
+    PLAYER_IA_GLIDER = 73,
+    /* 0x43 */ PLAYER_IA_MAX,
 
 } PlayerItemAction;
 
